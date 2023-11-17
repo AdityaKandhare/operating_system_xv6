6b7ce4c20d4e9d9ac6984b97b63eb0aa83e8c112
akandhare <akandhare@binghamton.edu>
2023-11-17 16:59:17 -0500

    Base code for xv6

---
+++ xv6-intro-BASE/myecho.c
@@ -0,0 +1,19 @@
+#include "types.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+    int i = 0;
+
+    printf(1, "***Print in user space: ");
+    
+    for (i = 1; i < argc; i++)
+    {
+        printf(1, argv[i]);
+        printf(1, " ");
+    }
+
+    printf(1, "\n");
+    
+    exit();
+}
